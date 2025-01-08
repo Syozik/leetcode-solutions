349a3a658955876295a080993ee05be274c5acf1
Serhii Rubanskyi <rubanskiysergiy@gmail.com>
2025-01-08 13:15:25 +0100

    Solved 20. Valid parentheses

---
+++ google/20.js
@@ -0,0 +1,37 @@
+class Stack {
+  constructor() {
+    this.parentheses = [];
+    this.pairs = {"(":")", "[":"]", "{":"}"};
+}
+
+  push(bracket) {
+    this.parentheses.push(bracket);
+  }
+
+  pop(bracket) {
+    
+    if (this.pairs[this.parentheses[this.parentheses.length - 1]] != bracket) return false;
+    return this.parentheses.pop();
+  }
+
+
+  isEmpty(){
+    return this.parentheses.length === 0;
+  }
+}
+
+const isValid = function(s){
+    let brackets = new Stack();
+    const opening = ["(", "[", "{"];
+    for (let i=0; i<s.length; i++){
+        if (opening.includes(s[i])){
+            brackets.push(s[i]);            
+        }
+        else if (brackets.pop(s[i])===false)
+            return false;
+    }
+
+    return brackets.isEmpty();
+}
+
+console.log(isValid("()[]"))
