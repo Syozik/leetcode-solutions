9ed55d613a67bd9346baabb17f447dbdede45484
Serhii Rubanskyi <rubanskiysergiy@gmail.com>
2025-01-08 17:50:14 +0100

    Add 215. Kth Largest Element in an Array

---
+++ amazon/215.ts
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+
+function findKthLargest(nums: number[], k: number): number {
+    nums.sort((a,b)=>a-b);
+    return nums[nums.length - k];
+};
+
+console.log(findKthLargest([3,2,3,1,2,4,5,5,6,7,7,8,2,3,1,1,1,10,11,5,6,2,4,7,8,5,6],2))
