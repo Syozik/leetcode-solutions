a8c1c2af6d0b1b3634bd9236f15598a54d8bf19d
Serhii Rubanskyi <rubanskiysergiy@gmail.com>
2025-01-11 12:49:27 +0100

    Solved 49. Group anagrams

--- 2021 Advent of code 11.Dumbo Octopus.ts
+++ 2021 Advent of code 11.Dumbo Octopus.ts
@@ -1,5 +1,5 @@
 let flashes: number = 0;
-const cleanEnergy = function(octopuses: [number[]]): void{
+const cleanEnergy = function(octopuses: number[][]): void{
     for (let i=0; i<octopuses.length; i++){
         for (let j=0; j<octopuses[i].length; j++){
             if (octopuses[i][j] >= 10){
@@ -8,7 +8,7 @@ const cleanEnergy = function(octopuses: [number[]]): void{
         }
     }
 }
-const addEnergy = function (octopuses: [number[]], i: number, j: number): void {
+const addEnergy = function (octopuses: number[][], i: number, j: number): void {
   octopuses[i][j] += 1;
   if (octopuses[i][j] === 10) {
     flashes += 1;
@@ -17,7 +17,7 @@ const addEnergy = function (octopuses: [number[]], i: number, j: number): void {
 };
 
 const flashNeighbors = function (
-  octopuses: [number[]],
+  octopuses: number[][],
   i: number,
   j: number
 ): void {
@@ -53,7 +53,7 @@ const flashNeighbors = function (
   }
 };
 
-const numberOfFlashes = function (octopuses: [number[]]): number {
+const numberOfFlashes = function (octopuses: number[][]): number {
   const [width, length]: [number, number] = [10, 10];
 
   for (let day = 0; day < 100; day++) {
@@ -67,16 +67,16 @@ const numberOfFlashes = function (octopuses: [number[]]): number {
   return flashes;
 };
 
-const stringToMatrix = function (octopuses: string): [number[]] {
+const stringToMatrix = function (octopuses: string): number[][] {
   let octopusesArray: string[] = octopuses.split("\n");
-  let octopusesMatrix: [number[]] = [[]];
+  let octopusesMatrix: number[][] = [[]];
   for (let i = 0; i < octopusesArray.length; i++) {
     octopusesMatrix[i] = octopusesArray[i].split("").map(Number);
   }
   return octopusesMatrix;
 };
 
-const octopuses: [number[]] = stringToMatrix(`1564524226
+const octopuses: number[][] = stringToMatrix(`1564524226
 1384554685
 7582264835
 8812672272
---
+++ google/49.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getAnagram(self, word):
+        letters = {}
+        for letter in word:
+            letters[letter] = letters.get(letter, 0) + 1     
+        return letters
+    
+    def groupAnagrams(self, strs):
+        anagrams = []
+        for word in strs:
+            anagram = self.getAnagram(word)
+            for i in range(len(anagrams)):
+                if anagrams[i][0] == anagram:
+                    anagrams[i][1].append(word)
+                    break
+            else:
+                anagrams.append([anagram, [word]])
+        return [words for [anagram, words] in anagrams]
