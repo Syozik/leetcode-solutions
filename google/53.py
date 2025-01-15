6982698b2e53723168337eae4dafe73310d25334
Serhii Rubanskyi <rubanskiysergiy@gmail.com>
2025-01-15 12:18:00 +0100

    Modified file

--- google/53.py
+++ google/53.py
@@ -5,14 +5,11 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         max_sum: int = max(nums)
         sum: int =  0
-        subarray = []
         for num in nums:
             if sum + num < 0:
-                subarray = []
                 sum = 0
                 continue
             sum += num
-            subarray.append(num)
             if sum >= max_sum:
                 max_sum = sum
         return max_sum
